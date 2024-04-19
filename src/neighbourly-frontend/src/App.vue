f8a4eea0e2bbe83ec17c0b6e72a6e3f515802aeb
Bogdan <nedbog@gmail.com>
2024-04-20 01:43:01 +0300

    Wip authenticate page

--- src/neighbourly-frontend/src/App.vue
+++ src/neighbourly-frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Authenticate from './components/Authenticate.vue'
 </script>
 
 <template>
@@ -8,11 +8,11 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Authenticate />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Log in</RouterLink>
+        <RouterLink to="/about">Register</RouterLink>
       </nav>
     </div>
   </header>
