0d0bc2945fd5dcf072facfab572af2d7da30f1d8
Bogdan Nedelcu <bogdan.nedelcu@ifacts.se>
2024-10-19 16:05:34 +0300

    remove favicon

--- src/neighbourly-frontend/src/router/index.ts
+++ src/neighbourly-frontend/src/router/index.ts
@@ -2,15 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Authenticate from '@/components/authentication/Authenticate.vue'
 import Login from '@/components/authentication/Login.vue'
 import Register from '@/components/authentication/Register.vue'
+import App from '@/App.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'authentication',
-      component: Authenticate
-    },
     {
       path: '/login',
       name: 'login',
