6cacfde501db928a0544d2a983d26dd29f26d56b
Bogdan Nedelcu <nedbog@gmail.com>
2024-10-20 11:40:34 +0300

    introduce primevue menubar

--- src/neighbourly-frontend/src/components/navigation/NavBar.vue
+++ src/neighbourly-frontend/src/components/navigation/NavBar.vue
@@ -1,10 +1,60 @@
 <script setup lang="ts">
+import Menubar from 'primevue/menubar';
+import { ref } from "vue";
+
+const items = ref([
+  {
+    label: 'Home',
+    icon: 'pi pi-home'
+  },
+  {
+    label: 'Features',
+    icon: 'pi pi-star'
+  },
+  {
+    label: 'Projects',
+    icon: 'pi pi-search',
+    items: [
+      {
+        label: 'Components',
+        icon: 'pi pi-bolt'
+      },
+      {
+        label: 'Blocks',
+        icon: 'pi pi-server'
+      },
+      {
+        label: 'UI Kit',
+        icon: 'pi pi-pencil'
+      },
+      {
+        label: 'Templates',
+        icon: 'pi pi-palette',
+        items: [
+          {
+            label: 'Apollo',
+            icon: 'pi pi-palette'
+          },
+          {
+            label: 'Ultima',
+            icon: 'pi pi-palette'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    label: 'Contact',
+    icon: 'pi pi-envelope'
+  }
+]);
+
 </script>
 
 <template>
-  <nav>
-    <h1>This is navigation, should really be replaced by something fancy!</h1>
-  </nav>
+  <div class="card">
+    <Menubar :model="items" />
+  </div>
 </template>
 
 <style scoped>
@@ -16,5 +66,13 @@ h1 {
 nav {
   width: 100vw;
   height: 0.05 * 100vh;
+}
+
+.card {
+  background: var(--card-bg);
+  border: var(--card-border);
+  padding: 2rem;
+  border-radius: 10px;
+  margin-bottom: 1rem;
 }
 </style>
