7331cc7d222ae30a18d4693bf991057082e1e65c
Bogdan Nedelcu <nedbog@gmail.com>
2024-10-20 11:47:17 +0300

    use Lara primevue theme

--- src/neighbourly-frontend/src/main.ts
+++ src/neighbourly-frontend/src/main.ts
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
+import Lara from '@primevue/themes/lara';
 
 import App from './App.vue'
 import router from './router'
@@ -12,23 +13,32 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue, { /* options */ });
+app.use(PrimeVue, {
+  theme: {
+    preset: Lara,
+    options: {
+      prefix: 'p',
+      darkModeSelector: 'system',
+      cssLayer: false
+    }
+  }
+});
 
 axios.interceptors.request.use(
-    (config) => {
-      const token = localStorage.getItem('neighbourly-jwt-token');
-      if (token) {
-        config.headers['Authorization'] = 'Bearer ' + token;
-      }
-      return config;
-    },
-    (error) => {
-      if (error.response.status === 401) {
-        router.push('/login');
-      }
-
-      return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem('neighbourly-jwt-token');
+    if (token) {
+      config.headers['Authorization'] = 'Bearer ' + token;
     }
-  );
-  
+    return config;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      router.push('/login');
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 app.mount('#app')
